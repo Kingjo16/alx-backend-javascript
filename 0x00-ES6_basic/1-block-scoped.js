8e729310b95419500f0b8e6b84808f56cb96b188
Kidus Yohannes <kidusyohannesred@gmail.com>
2024-07-01 06:18:34 +0000

    number 1 fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task = true; // Block-scoped variable
+    const task2 = false; // Block-scoped variable
   }
 
   return [task, task2];
